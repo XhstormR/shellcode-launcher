97b6daeae0ec370b464e0301ee5687f642474191
XhstormR <10527522+XhstormR@users.noreply.github.com>
2019-10-07 17:00:15 +0800

    Signed-off-by: XhstormR <10527522+XhstormR@users.noreply.github.com>

--- assets/run.sh
+++ assets/run.sh
@@ -1,3 +1,3 @@
-xxd -p payload.raw | tr -d "\n" > payload.txt && split -n 10 -e -d --additional-suffix=.txt payload.txt payload-
+xxd -p payload.raw | tr -d "\n" > payload.txt && split -n 50 -e -d --additional-suffix=.txt payload.txt payload-
 
 # cat payload-*
