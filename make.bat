b44e3b64b49e10cea7bae80f90c452f23fffb1b6
XhstormR <10527522+XhstormR@users.noreply.github.com>
2019-12-19 21:00:19 +0800

    Signed-off-by: XhstormR <10527522+XhstormR@users.noreply.github.com>

--- make.bat
+++ make.bat
@@ -4,5 +4,5 @@ if exist cmake-build-default rd /q /s cmake-build-default
 if not exist cmake-build-default md cmake-build-default
 pushd cmake-build-default
 cmake .. -G "MinGW Makefiles" %*
-mingw32-make install
+mingw32-make install package
 popd
